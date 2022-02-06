453736f76ac96374441b4f8560bd7517c3898a38
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-02-06 17:57:32 -0600

    heroku updated

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals');
 
@@ -64,6 +65,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
 });
